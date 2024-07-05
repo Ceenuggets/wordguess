1a9965418827fb72a0082bba2ef9e9ccf131064e
Ray Eke <NACO40006@naoc.pri>
2024-07-05 16:33:41 +0100

    text selection

--- word_guess.py
+++ word_guess.py
@@ -4,70 +4,75 @@ import dash_bootstrap_components as dbc
 import re
 import random
 
+
 external_stylesheets = [dbc.themes.BOOTSTRAP, '/assets/word_guess.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
                 meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
 
-guess_letters = set()
+random_output = ""
+masked_random_output= ""
 num = 0
-guessed_word = ""
+guessed_letters = set()
+match_found = False
 
 
-class WordGuess:
-    def __init__(self, *words):
-        self.words = words
-        self.correct_letters_count = 0
-        self.chosen_word = set()
-        self.guessed_word = ""
-        self.guessed_letters = set()
-
-    def random_word(self):
-        random_word = random.choice(self.words)
-        self.guessed_word = re.sub(r".", "-", random_word)
-        return random_word
+def random_pick(*words):
+    global num
+    global guessed_letters
+    global match_found
+    random_word = random.choice(*words)
+    rnd_mask = re.sub(r".", "-", random_word)
+    num = 0
+    match_found = False
+    guessed_letters.clear()
+    return random_word, rnd_mask
 
 
-def compare_words(current_word, letter):
-    global guessed_word
-    for i in range(len(current_word)):
-        if current_word[i] == letter.lower():
-            new_word = list(guessed_word)
-            new_word[i] = letter.lower()
-            guessed_word = "".join(new_word)
-    return guessed_word
+def word_screen(input_char):
+    global masked_random_output
+    for i in range(len(random_output)):
+        if random_output[i].lower() == input_char.lower():
+            new_word = list(masked_random_output)
+            new_word[i] = input_char.lower()
+            masked_random_output = "".join(new_word)
+            print("".join(new_word))
+    return masked_random_output
 
 
-sample = WordGuess("Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
-                   "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
-                   "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan",
-                   "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria",
-                    "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada",
-                    "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros",
-                    "Republic of the Congo", "Congo DRC", "Costa Rica", "Ivory Coast",
-                    "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica",
-                    "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
-                    "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France",
-                    "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala",
-                    "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland",
-                    "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-                    "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan",
-                    "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein",
-                    "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali",
-                    "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
-                    "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar",
-                    "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger",
-                    "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau",
-                    "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland",
-                    "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis",
-                    "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino",
-                    "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles",
-                    "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia",
-                    "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan",
-                    "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand",
-                    "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-                    "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates",
-                    "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela",
-                    "Vietnam", "Yemen", "Zambia", "Zimbabwe")
+sample = "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
+"Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
+"Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan",
+"Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria",
+"Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada",
+"Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros",
+"Republic of the Congo", "Congo DRC", "Costa Rica", "Ivory Coast",
+"Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica",
+"Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
+"Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France",
+"Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala",
+"Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland",
+"India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
+"Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan",
+"Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein",
+"Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali",
+"Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
+"Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar",
+"Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger",
+"Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau",
+"Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland",
+"Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis",
+"Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino",
+"Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles",
+"Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia",
+"South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan",
+"Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand",
+"Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
+"Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates",
+"United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela",
+"Vietnam", "Yemen", "Zambia", "Zimbabwe"
+
+
+
 
 app.layout = dbc.Container(children=[
     dbc.Row([
@@ -79,10 +84,11 @@ app.layout = dbc.Container(children=[
                 html.Div([
                     html.Div([
                         html.Div([
-                            html.Label("Country:", style={'fontSize': '18px', 'marginRight': '5px', 'fontWeight': 'bold'}),
+                            html.Label("Country:",
+                                       style={'fontSize': '18px', 'marginRight': '5px', 'fontWeight': 'bold'}),
                             html.Span(id="expected_word"),
                         ]),
-                        html.Hr(style={"width": "100% !important",  }),
+                        html.Hr(style={"width": "100% !important", }),
                         html.Span(id="min_expected_guess"),
                         html.Span(id="guessed_letters"),
                         html.Span(id="num_of_attempts"),
@@ -97,7 +103,7 @@ app.layout = dbc.Container(children=[
                         style={'margin': '10px'},
                         maxLength=1,
                         pattern=".{1,1}",
-                       
+
                     ),
                 ])
             ], id="input_div")
@@ -105,9 +111,61 @@ app.layout = dbc.Container(children=[
             className="mx-auto"
         ),
     ], justify="center", className='g-0'),
-    dcc.Store(id='stored_data'),
+    dcc.Store(id='input_interacted', data=False),
     dcc.Store(id='store-content', data=None),
 ], fluid=True)
+
+
+
+
+@app.callback(
+    Output('input_interacted', 'data'),
+    Input('user_input', 'value'),
+    State('input_interacted', 'data')
+)
+def track_input_interaction(value, interacted):
+    if not interacted and value:
+        return True
+    return interacted
+
+
+@app.callback(
+    Output('expected_word', 'children'),
+    Output("min_expected_guess", "children"),
+    Output("guessed_letters", "children"),
+    Output("num_of_attempts", "children"),
+    Output("expected_word", "style"),
+    [Input('user_input', 'value'),
+     Input('input_interacted', 'data')]
+)
+def update_output(value, interacted):
+    global random_output
+    global masked_random_output
+    global num
+    global match_found
+    pattern = re.compile(r"^[a-zA-Z\s.]+$")
+    if interacted:
+        if match_found:
+            random_output, masked_random_output = random_pick(sample)
+            return masked_random_output, html.P(["Minimum expected guesses: ", html.Span(len(random_output), className="min-attempts shared-span-style")]),html.P(["Already guessed: ", html.Span(",".join(guessed_letters), className="guessed_letters shared-span-style")]),html.P(["Attempts: ", html.Span(str(num), className="attempts shared-span-style")]),{'backgroundColor': '#f0f0f0', 'color': 'black', 'boxShadow': 'box-shadow: 5px 10px 5px rgba(0, 0, 0, 0.2)'}
+        if pattern.match(value):
+            num += 1
+            guessed_letters.update(value.lower())
+            guess_outcome = word_screen(value)
+            if guess_outcome.lower() == random_output.lower():
+                match_found = True
+                # print("Yes, you've got the answer!")
+                print(num)
+                print(guessed_letters)
+                return random_output, html.P(["Minimum expected guesses: ", html.Span(len(random_output), className="min-attempts shared-span-style")]),html.P(["Already guessed: ", html.Span(",".join(guessed_letters), className="guessed_letters shared-span-style")]),html.P(["Attempts: ", html.Span(str(num), className="attempts shared-span-style")]),{'backgroundColor': 'green', 'color': 'white', 'boxShadow': '5px 10px 5px rgba(0, 0, 0, 0.4)', 'border': '2px solid white'}
+            else:
+                return guess_outcome, html.P(["Minimum expected guesses: ", html.Span(len(random_output), className="min-attempts shared-span-style")]), html.P(["Already guessed: ", html.Span(",".join(guessed_letters), className="guessed_letters shared-span-style")]),html.P(["Attempts: ", html.Span(str(num), className="attempts shared-span-style")]),{'backgroundColor': '#f0f0f0', 'color': 'black', 'boxShadow': 'box-shadow: 5px 10px 5px rgba(0, 0, 0, 0.2)'}
+        else:
+            return masked_random_output, html.P(["Minimum expected guesses: ", html.Span(len(random_output), className="min-attempts shared-span-style")]), html.P(["Already guessed: ", html.Span(",".join(guessed_letters), className="guessed_letters shared-span-style")]),html.P(["Attempts: ", html.Span(str(num), className="attempts shared-span-style")]), {'backgroundColor': '#f0f0f0', 'color': 'black', 'boxShadow': 'box-shadow: 5px 10px 5px rgba(0, 0, 0, 0.2)'}
+    else:
+        random_output, masked_random_output = random_pick(sample)
+        # masked_random_output = re.sub(r".", "-", random_output)
+        return masked_random_output, html.P(["Minimum expected guesses: ", html.Span(len(random_output), className="min-attempts shared-span-style")]), html.P(["Already guessed: ", html.Span("-", className="guessed_letters shared-span-style")]),html.P(["Attempts: ", html.Span(str(num), className="attempts shared-span-style")]), {'backgroundColor': '#f0f0f0', 'color': 'black', 'boxShadow': 'box-shadow: 5px 10px 5px rgba(0, 0, 0, 0.2)'}
 
 
 app.clientside_callback(
@@ -125,63 +183,5 @@ app.clientside_callback(
     Input('user_input', 'value')
 )
 
-@app.callback(
-    Output("expected_word", "children"),
-    Output("min_expected_guess", "children"),
-    Output('stored_data', 'data'),
-    Input("user_input", "value"),
-    State("expected_word", "children"),
-)
-def update_output_div(user_input, current_word):
-    global guessed_word
-    chosen_word = set()
-    if not current_word:
-        new_word = sample.random_word()
-        chosen_word.update(new_word)
-        stored_data = {'current_word': new_word}
-        masked_newword = re.sub(r".", "-", new_word)
-        guessed_word = masked_newword
-        # print(new_word)
-        return masked_newword, html.P(["Minimum expected guesses: ", html.Span(len(chosen_word), className="min-attempts shared-span-style")]), stored_data
-    else:
-        return dash.no_update
-
-
-@app.callback(
-    Output("expected_word", "children",  allow_duplicate=True),
-    Output("guessed_letters", "children"),
-    Output("num_of_attempts", "children"),
-    Output("expected_word", "style"),
-    Input("user_input", "value"),
-    State('stored_data', 'data'),
-    prevent_initial_call=True,
-)
-def process_user_input(user_input, stored_data):
-    global num
-    global guessed_word
-    pattern = re.compile(r"^[a-zA-Z\s.]+$")
-    if pattern.match(user_input):
-        num += 1
-        if stored_data:
-            current_word = stored_data['current_word'].lower()
-            guess_letters.update(user_input.lower())
-            guess_outcome = compare_words(current_word, user_input.lower())
-
-            if guess_outcome.lower() == current_word.lower():
-                num = 0
-                guess_letters.clear()
-                # print("Hurray, You have answer")
-                return stored_data['current_word'], html.P(["Already guessed: ", html.Span(",".join(guess_letters), className="guessed_letters shared-span-style")]), html.P(["Attempts: ", html.Span(str(num), className="attempts shared-span-style")]), {'backgroundColor': 'green', 'color': 'white', 'boxShadow': '5px 10px 5px rgba(0, 0, 0, 0.4)', 'border': '2px solid white'}
-                
-            else:
-                return guess_outcome, html.P(["Already guessed: ", html.Span(",".join(guess_letters), className="guessed_letters shared-span-style")]), html.P(["Attempts: ", html.Span(str(num),className="attempts shared-span-style") ]), {'backgroundColor': '#f0f0f0', 'color': 'black', 'boxShadow': 'box-shadow: 5px 10px 5px rgba(0, 0, 0, 0.2)'}
-
-            # return guessed_word, f"Already guessed letters: {",".join(guess_letters)}", f"Number of attempts: {num}"
-
-        else:
-            return dash.no_update
-    return dash.no_update
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True, port=8066)
+if __name__ == '__main__':
+    app.run_server(debug=True)
